9b01d8ea5c1fb50e07da86eaec50cc1c7dfd6e24
Camila Lo Nero <lonerocamila@gmail.com>
2023-10-17 11:05:58 -0300

    [added] validations of user, post, recipe

--- server/src/middlewares/validatorSchema.middleware.ts
+++ server/src/middlewares/validatorSchema.middleware.ts
@@ -1,10 +1,49 @@
-import joi from "@hapi/joi";
+import zod from "zod";
 
-const userSchema = joi.object({
-  username: joi.string().alphanum().min(3).max(30).required(),
-  password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")), // 3-30 caracteres, solo numeros y letras
-  email: joi.string().email(),
-  age: joi.number().integer().min(0),
+const hasSpecialCharacter = (value: string) =>
+  /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(value);
+
+export const userSchema = zod.object({
+  username: zod
+    .string()
+    .min(5, "El nombre debe contener al menos 5 caracteres")
+    .max(40, "El nombre debe contener menos de 40 caracteres"),
+  email: zod
+    .string()
+    .email()
+    .refine(
+      (value) => value.includes("@"),
+      "El correo electrónico debe contener el carácter '@'."
+    ),
+  password: zod
+    .string()
+    .min(5)
+    .max(20) //contraseña mas segura
+    .refine((value) => hasSpecialCharacter(value), {
+      message: "La contraseña debe contener al menos un carácter especial.",
+      path: [], // la ruta del error, en este caso la raíz de la cadena
+    }),
 });
 
-export default userSchema;
+export const postSchema = zod.object({
+  title: zod
+    .string()
+    .min(5, "El titulo debe contener al menos 5 caracteres")
+    .max(40, "El titulo debe contener menos de 40 caracteres"),
+});
+
+export const recipeSchema = zod.object({
+  title: zod
+    .string()
+    .min(5, "El titulo debe contener al menos 5 caracteres")
+    .max(40, "El titulo debe contener menos de 40 caracteres"),
+  description: zod.string().min(5).max(50),
+  servings: zod.number().min(5).max(50),
+  ingredients: zod.array(zod.string().min(2).max(100)), // Convertido a array
+  steps: zod.array(zod.string().min(5).max(200)), // Convertido a array y aumentado el máximo
+  image: zod.string().min(5).max(50),
+  category: zod.string().min(5).max(50),
+  readyMinutes: zod.number().min(5).max(50),
+  portions: zod.number().min(5).max(50),
+  tags: zod.string().min(5).max(50),
+});
