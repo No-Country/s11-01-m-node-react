85fd1924ff68e1c4dd8d96403f5b40efb80b8945
Your Name <you@example.com>
2023-10-17 12:53:15 -0300

    Cambios en users.services y auth.controller

--- server/src/controllers/auth.controllers.ts
+++ server/src/controllers/auth.controllers.ts
@@ -3,24 +3,27 @@ import { v4 as uuidv4 } from 'uuid';
 import addRefreshTokenToWhitelist from "../services/auth.services";
 import { generateTokens } from '../utils/jwt';
 
-const {
-    findUserByEmail,
-    createUserByEmailAndPassword,
-} = require('../services/users.services')
+import {
+    createUser,
+    getUserByEmail,
+} from '../services/users.services';
 
-const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const { email, password } = req.body;
         if (!email || !password) {
             res.status(404);
             throw new Error('You must provide an email and a password.');
         }
-        const existingUser = await findUserByEmail(email);
+        const existingUser = await getUserByEmail(email);
         if (existingUser) {
             res.status(400);
             throw new Error('Email is already in use');
         }
-        const user = await createUserByEmailAndPassword({ email, password });
+        const user = await createUser({
+            email, password,
+            username: ""
+        });
         const jti = uuidv4();
         const { accessToken, refreshToken } = generateTokens(user, jti);
         await addRefreshTokenToWhitelist.addRefreshTokenToWhitelist({ jti, refreshToken, userId: user.id });
@@ -34,4 +37,4 @@ const register = async (req: Request, res: Response, next: NextFunction): Promis
     }
 }
 
-export default register;
+
