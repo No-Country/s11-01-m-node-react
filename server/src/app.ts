e281406df33a9733f060d6975d6d533de098d54a
Camila Lo Nero <lonerocamila@gmail.com>
2023-10-18 15:58:23 -0300

    [add] searchRoute

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import router from "./routes/index";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use("v1", router);
 export default app;
--- server/src/routes/search.routes.ts
+++ server/src/routes/search.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-
+import { searchController } from "../controllers/search.controllers";
 const router = Router();
 
-// router.get("/search", searchController);
+router.get("/search", searchController);
 
 export default router;
