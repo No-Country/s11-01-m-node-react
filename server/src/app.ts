fa13bc26f8b3d9e0be3ec7e05d137fac51180e05
Your Name <you@example.com>
2023-10-18 20:33:03 -0300

    cambios en app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express"
-
+import authRoutes from '../src/routes/auth.routes'
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/v1', authRoutes)
+
 
 export default app
