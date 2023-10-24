c503123187aeab5f1aefe3c1af8147db9be16eb1
Adrian <esteban.adrian.gonz@gmail.com>
2023-10-24 10:47:47 -0300

    fix: process.env.PORT

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import http from "http";
 import app from "./app";
 
-const PORT = 8001;
+const PORT = process.env.PORT || 8001;
 const server = http.createServer(app);
 
 const startServer = () => {
