51cda6fd9e49a6652c234e26a76e265fd451f4d8
Camila Lo Nero <lonerocamila@gmail.com>
2023-10-26 09:51:30 -0300

    search.routes.test.ts 🚀

---
+++ server/test/routes/search.routes.test.ts
@@ -0,0 +1,30 @@
+import express, { Express } from "express";
+import router from "../../src/routes/search.routes";
+import request from "supertest";
+
+let app: Express;
+
+beforeAll(() => {
+  app = express();
+  app.use(express.json());
+  app.use(router);
+});
+
+describe("Search Routes", () => {
+  const testSearch = [
+    {
+      input: {
+        name: "  ",
+        ingredients: "  ",
+        instructions: "  ",
+      },
+      expectedResult: 200,
+    },
+  ];
+  testSearch.forEach(({ input, expectedResult }, index) => {
+    it(`should handle search - Test Case: ${index + 1}`, async () => {
+      const response = await request(app).post("/search").send(input);
+      expect(response.status).toBe(expectedResult);
+    });
+  });
+});
