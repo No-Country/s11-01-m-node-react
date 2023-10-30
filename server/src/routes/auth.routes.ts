363679647f9977c918f0f7fe3c54c3262a9ef93e
Your Name <you@example.com>
2023-10-30 14:23:21 -0300

    Se creo createRecipe.controller,requireAuth.middleware y se agrego a auth.routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,7 +1,9 @@
 import express, { Router } from 'express';
 import passport from 'passport';
 import { login, logout, register } from '../controllers/auth.controllers';
+import { createRecipe } from '../controllers/createRecipe.controllers';
 import { redirectIfLoggedIn } from '../middlewares/auth.middleware';
+import { requireAuth } from '../middlewares/requireAuth.middleware';
 
 
 const router: Router = express.Router();
@@ -9,6 +11,7 @@ const router: Router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.post('/login', redirectIfLoggedIn, login);
+router.post('/createRecipe', requireAuth, createRecipe)
 router.post('/logout', passport.authenticate('jwt', { session: false }), logout);
 
 export default router;
